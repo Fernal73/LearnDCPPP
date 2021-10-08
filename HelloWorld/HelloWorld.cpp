6128ab214919e875bfe92ecb978bdf725d4d971d
fernal73 <linus.fernandes@gmail.com>
2021-10-09 04:13:24 +0530

    Add HelloWorld example

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,26 @@
+/*
+ * HelloWorld.cpp
+ * Copyright (C) 2021  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#include <CL/sycl.hpp> 
+#include <iostream>
+using namespace sycl;
+
+const std::string secret {
+"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
+"J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
+
+const auto sz = secret.size();
+
+int main() {
+queue Q;
+char*result = malloc_shared<char>(sz, Q);
+std::memcpy(result,secret.data(),sz);
+Q.parallel_for(sz,[=](auto&i) {
+result[i] -= 1;
+}).wait();
+std::cout << result << "\n";
+return 0;
+}
