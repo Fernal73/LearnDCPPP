c8fb6750e476f9ef441d6da64dbd4ac0cdbeacb9
fernal73 <linus.fernandes@gmail.com>
2021-10-09 21:33:01 +0530

    Add call to display bound device

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,23 +4,25 @@
  *
  * Distributed under terms of the MIT license.
  */
-#include <CL/sycl.hpp> 
+#include <CL/sycl.hpp>
 #include <iostream>
 using namespace sycl;
 
-const std::string secret {
-"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
-"J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
+const std::string secret{"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
+                         "J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
 
 const auto sz = secret.size();
 
 int main() {
-queue Q;
-char*result = malloc_shared<char>(sz, Q);
-std::memcpy(result,secret.data(),sz);
-Q.parallel_for(sz,[=](auto&i) {
-result[i] -= 1;
-}).wait();
-std::cout << result << "\n";
-return 0;
+  queue Q;
+  cout << "Selected device: " << Q.get_device().get_info<info::device::name>()
+       << "\n";
+
+  char *result = malloc_shared<char>(sz, Q);
+
+  memcpy(result, secret.data(), sz);
+
+  Q.parallel_for(sz, [=](auto &i) { result[i] -= 1; }).wait();
+  std::cout << result << "\n";
+  return 0;
 }
