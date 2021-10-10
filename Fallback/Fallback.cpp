4f84cb0ac74914079e10069bcb6b769351c09d8a
fernal73 <linus.fernandes@gmail.com>
2021-10-10 22:05:27 +0530

    Add buf

--- Fallback/Fallback.cpp
+++ Fallback/Fallback.cpp
@@ -31,7 +31,8 @@ int main() {
                        });
       },
       cpuQueue);
-  auto bufacc1 = buf.get_access<access::mode::read>();
+  //auto bufacc1 = buf.get_access<access::mode::read>();
+  host_accessor bufacc1{buf};
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       if (bufacc1[i][j] != i + j) {
