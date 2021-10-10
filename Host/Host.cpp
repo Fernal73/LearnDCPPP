ab6b7cbd5deb6edbcdcac878492a81a036a2e8b0
fernal73 <linus.fernandes@gmail.com>
2021-10-10 11:32:56 +0530

    Add std

--- Host/Host.cpp
+++ Host/Host.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 int main() { // Create queue to use the host device explicitly queue Q{
              // host_selector{} };
-  cout << "Selected device: " << Q.get_device().get_info<info::device::name>()
+  std::cout << "Selected device: " << Q.get_device().get_info<info::device::name>()
        << "\n";
-  cout << " -> Device vendor: "
+  std::cout << " -> Device vendor: "
        << Q.get_device().get_info<info::device::vendor>() << "\n";
   return 0;
 }
