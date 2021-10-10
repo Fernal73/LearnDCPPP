c45b9fb42a1e973cdafb0664cfc27f91895b8c63
fernal73 <linus.fernandes@gmail.com>
2021-10-10 10:07:37 +0530

    renove std

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -7,8 +7,9 @@
 #include <CL/sycl.hpp>
 #include <iostream>
 using namespace sycl;
+using namespace std;
 
-const std::string secret{"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
+const string secret{"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
                          "J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
 
 const auto sz = secret.size();
@@ -23,6 +24,6 @@ int main() {
   memcpy(result, secret.data(), sz);
 
   Q.parallel_for(sz, [=](auto &i) { result[i] -= 1; }).wait();
-  std::cout << result << "\n";
+  cout << result << "\n";
   return 0;
 }
