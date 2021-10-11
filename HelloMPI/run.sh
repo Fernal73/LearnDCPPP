a16a0f5715e5ee5dc187952c40f9a403c8c844fe
fernal73 <linus.fernandes@gmail.com>
2021-10-11 16:25:46 +0530

    remove resource spec

--- HelloMPI/run.sh
+++ HelloMPI/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#PBS -l nodes=8:ppn=2
+#PBS -l nodes=4:ppn=2
 cd $PBS_O_WORKDIR
 echo Launching the parallel job from mother superior `hostname`...
 mpirun -machinefile $PBS_NODEFILE ./HelloMPI
