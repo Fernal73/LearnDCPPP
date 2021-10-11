f1b982f821978136215eeed86c85544852a9f72b
fernal73 <linus.fernandes@gmail.com>
2021-10-11 16:21:26 -0700

    Add debug commands to run.sh. modify make to ignore local execution on ssh login

--- HelloMPI/run.sh
+++ HelloMPI/run.sh
@@ -3,5 +3,8 @@
 #PBS -l nodes=2:ppn=2
 
 cd $PBS_O_WORKDIR
-echo Launching the parallel job from mother superior `hostname`...
+pwd
+echo Launching the parallel job from mother superior $(hostname)...
+echo "$PBS_NODEFILE"
+cat "$PBS_NODEFILE"
 mpirun -machinefile $PBS_NODEFILE ./HelloMPI
