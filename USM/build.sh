9b8f6bc57e0ae58be8ec5ca0803fb48affe18de1
fernal73 <linus.fernandes@gmail.com>
2021-10-12 09:43:41 +0530

    change node type

--- USM/build.sh
+++ USM/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #PBS -q batch
-#PBS -l nodes=1:gpu:ppn=2
-
+#PBS -l nodes=1:ppn=2
 source /opt/intel/inteloneapi/setvars.sh
 make clean
 make all
--- USM/run.sh
+++ USM/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #PBS -q batch
-#PBS -l nodes=1:gpu:ppn=2
+#PBS -l nodes=1:ppn=2
 source /opt/intel/inteloneapi/setvars.sh
 make run
