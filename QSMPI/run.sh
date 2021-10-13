af082ca79ca390a4552911fab362a0d873a345c6
fernal73 <linus.fernandes@gmail.com>
2021-10-13 09:34:06 +0530

    Add QSMPI

---
+++ QSMPI/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#PBS -q batch
+#PBS -l nodes=2:ppn=2
+
+cd $PBS_O_WORKDIR
+pwd
+echo Launching the parallel job from mother superior $(hostname)...
+echo "$PBS_NODEFILE"
+cat "$PBS_NODEFILE"
+mpirun -machinefile $PBS_NODEFILE ./HelloMPI
+mpirun -machinefile $PBS_NODEFILE ./HelloMPI2
+cat "$PBS_NODEFILE" | head -n 2 > nodes.txt
+mpirun -machinefile nodes.txt ./SendRecv
