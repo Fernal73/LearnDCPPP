3d2fcd0c2e2a51c1579276847de7c9b98fc27812
fernal73 <linus.fernandes@gmail.com>
2021-10-14 20:52:53 +0530

    rm fpga files

--- Host/build_fpga_emu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source /opt/intel/inteloneapi/setvars.sh
-make fpga_emu -f Makefile.fpga
-make cpu_fpga_emu -f Makefile.fpga
--- Host/run_fpga_hw.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source /opt/intel/inteloneapi/setvars.sh
-make run_hw -f Makefile.fpga
-make cpu_run_hw -f Makefile.fpga
-make sel_run_hw -f Makefile.fpga
-make que_run_hw -f Makefile.fpga
