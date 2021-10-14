62558c61537f18074d677e51ef61a09b20c211b9
fernal73 <linus.fernandes@gmail.com>
2021-10-14 15:37:51 +0530

    Fix compilation errors

--- Queues/RAW.cpp
+++ Queues/RAW.cpp
@@ -5,49 +5,46 @@
  * Distributed under terms of the MIT license.
  */
 
-#include <CL/sycl.hpp> 
-#include <array> 
+#include <CL/sycl.hpp>
+#include <array>
 using namespace sycl;
 constexpr int N = 42;
-int main() { 
-  std::array<int,N> a, b, c;
-for (int i = 0; i < N; i++) { 
-  a[i] = b[i] = c[i] = 0;
-} 
+int main() {
+  std::array<int, N> a, b, c;
+  for (int i = 0; i < N; i++) {
+    a[i] = b[i] = c[i] = 0;
+  }
 
-queue Q;
-// We will learn how to simplify this example later 
-buffer A{a};
-buffer B{b};
-buffer C{c};
-Q.submit([&](handler &h) { 
+  queue Q;
+  // We will learn how to simplify this example later
+  buffer A{a};
+  buffer B{b};
+  buffer C{c};
+  Q.submit([&](handler &h) {
     accessor accA(A, h, read_only);
     accessor accB(B, h, write_only);
-    h.parallel_for( // computeB 
-        N, [=](id<1> i) { 
-        accB[i] = accA[i] + 1; 
-        });
-});
-Q.submit([&](handler &h) { 
+    h.parallel_for( // computeB
+        N, [=](id<1> i) { accB[i] = accA[i] + 1; });
+  });
+  Q.submit([&](handler &h) {
     accessor accA(A, h, read_only);
-    h.parallel_for( // readA 
-        N, [=](id<1> i) { 
-        // Useful only as an example 
-        int data = accA[i];
-});
-});
-Q.submit([&](handler &h) { 
-    // RAW of buffer B 
+    h.parallel_for( // readA
+        N, [=](id<1> i) {
+          // Useful only as an example
+          int data = accA[i];
+        });
+  });
+  Q.submit([&](handler &h) {
+    // RAW of buffer B
     accessor accB(B, h, read_only);
     accessor accC(C, h, write_only);
-h.parallel_for( // computeC N, [=](id<1> i) { accC[i] = accB[i] + 2; });
-});
-// read C on host host_accessor host_accC(C, read_only);
-for (int i = 0; i < N; i++) { 
-  std::cout << host_accC[i] << " ";
-} 
-std::cout << "\n";
-return 0;
+    h.parallel_for( // computeC
+        N, [=](id<1> i) { accC[i] = accB[i] + 2; });
+  });
+  // read C on host host_accessor host_accC(C, read_only);
+  for (int i = 0; i < N; i++) {
+    std::cout << host_accC[i] << " ";
+  }
+  std::cout << "\n";
+  return 0;
 }
-
-
