3fa24e7e0179974e3026fccc02ea476a2991aee9
fernal73 <linus.fernandes@gmail.com>
2021-10-15 11:11:33 +0530

    Add clean up fpga files

--- VectorAdd/buildfpga.sh
+++ VectorAdd/buildfpga.sh
@@ -2,5 +2,4 @@
 #PBS -q batch
 #PBS -l nodes=1:fpga_compile:ppn=2
 source /opt/intel/inteloneapi/setvars.sh
-make -f Makefile.fpga clean
 make -f Makefile.fpga fpga_emu
--- VectorAdd/buildfpgahw.sh
+++ VectorAdd/buildfpgahw.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 #PBS -l nodes=1:fpga_compile:ppn=2
 source /opt/intel/inteloneapi/setvars.sh
-make -f Makefile.fpga clean
 make -f Makefile.fpga hw
