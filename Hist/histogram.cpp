75c70dfcb727ef72506e2ad7987191e6151629bb
fernal73 <linus.fernandes@gmail.com>
2021-10-19 11:04:01 +0530

    Add output list

--- Hist/histogram.cpp
+++ Hist/histogram.cpp
@@ -38,6 +38,7 @@ void dense_histogram(std::vector<uint64_t> &input) {
     num_bins = histogram[N - 1] + 1;
   }
   cl::sycl::buffer<uint64_t, 1> histogram_new_buf{cl::sycl::range<1>(num_bins)};
+  cl::sycl::buffer<uint64_t, 1> bins{cl::sycl::range<1>(num_bins)};
   auto val_begin = oneapi::dpl::counting_iterator<int>{0};
 
   // Determine the end of each bin of value
@@ -46,20 +47,15 @@ void dense_histogram(std::vector<uint64_t> &input) {
       oneapi::dpl::end(histogram_buf), val_begin, val_begin + num_bins,
       oneapi::dpl::begin(histogram_new_buf));
 
-    sycl::host_accessor histogram_new(histogram_new_buf, sycl::read_only);
-    std::cout << "[";
-    for (int i = 0; i < num_bins; i++) 
-      std::cout << "(" << i << ", " << histogram_new[i] << ") ";
-    std::cout << "]\n";
   // Compute histogram by calculating differences of cumulative histogram
   std::adjacent_difference(oneapi::dpl::execution::dpcpp_default,
                            oneapi::dpl::begin(histogram_new_buf),
                            oneapi::dpl::end(histogram_new_buf),
-                           oneapi::dpl::begin(histogram_new_buf));
+                           oneapi::dpl::begin(bins));
 
   std::cout << "Dense Histogram:\n";
   {
-    sycl::host_accessor histogram_new(histogram_new_buf, sycl::read_only);
+    sycl::host_accessor histogram_new(bins, sycl::read_only);
     std::cout << "[";
     for (int i = 0; i < num_bins; i++) {
       std::cout << "(" << i << ", " << histogram_new[i] << ") ";
@@ -113,6 +109,7 @@ void sparse_histogram(std::vector<uint64_t> &input) {
 int main(void) {
   const int N = 1000;
   std::vector<uint64_t> input;
+  std::vector<uint64_t> dense, sparse;
   srand((unsigned)time(0));
   // initialize the input array with randomly generated values between 0 and 9
   for (int i = 0; i < N; i++)
@@ -128,10 +125,12 @@ int main(void) {
   for (int i = 0; i < N; i++)
     std::cout << input[i] << " ";
   std::cout << "\n";
-  dense_histogram(input);
+  dense = input;
+  dense_histogram(dense);
   for (int i = 0; i < N; i++)
-    std::cout << input[i] << " ";
+    std::cout << dense[i] << " ";
   std::cout << "\n";
-  sparse_histogram(input);
+  sparse = input;
+  sparse_histogram(sparse);
   return 0;
 }
